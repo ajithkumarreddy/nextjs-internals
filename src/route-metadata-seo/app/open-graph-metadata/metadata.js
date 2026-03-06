d7fdbc47e77d8d0260bcf5107f6caa093255c254
Ajith Kumar <ajithkumarreddy123@gmail.com>
2026-03-06 23:10:22 +0530

    added opengraph metadata

---
+++ src/route-metadata-seo/app/open-graph-metadata/metadata.js
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: "Blog",
+  description: "Latest posts",
+  openGraph: {
+    title: "Blog",
+    description: "Latest posts",
+    url: "https://example.com/blog",
+    siteName: "NextJS learning",
+    images: [
+      {
+        url: "https://example.com/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+};
